fb382f74612132c0d33a259531ada560bbd737dd
ninom5 <nmilas00@fesb.hr>
2025-02-02 01:52:23 +0100

    start with 8th task

---
+++ task8/task8.js
@@ -0,0 +1,4 @@
+const nameOfEquipment = document.querySelector("#name-input");
+const priceOfTheEquipment = document.querySelector("#price-input");
+const statusOfTheEquipment = document.querySelector("#status-input");
+const submitButton = document.querySelector(".submit-btn");
